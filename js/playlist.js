ae8afaf2f00e2be225e30cf8b29d5aa4450b00ec
Kristaps <kriforce7@gmail.com>
2021-10-27 15:58:35 +0300

    init: create drum kit

---
+++ js/playlist.js
@@ -0,0 +1,24 @@
+const sounds = ['car0', 'car1', 'car2', 'car3', 'car4', 'car5', 'car6', 'car7', 'car8']
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs() {
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0;
+    })
+}
